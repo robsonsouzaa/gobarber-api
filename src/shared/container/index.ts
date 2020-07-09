9157db0f821ec988358285d91be03236420d4ab1
Robson Souza <robsonex@gmail.com>
2020-07-08 21:34:05 -0400

    feat: testes de recuperação de senha

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -4,10 +4,13 @@ import '@modules/users/providers';
 import './providers';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
-import IUsersRepository from '@modules/users/repositories/IUsersRepository';
-
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+
+// import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+// import UserTokensRepository from '@modules/users/infra/typeorm/repositories/';
 
 container.registerSingleton<IAppointmentsRepository>(
   'AppointmentsRepository',
