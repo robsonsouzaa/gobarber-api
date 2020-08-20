c4a3abcf6e90ffeb9e9751e7edd21417638d1dcb
Robson Souza <robsonex@gmail.com>
2020-08-20 13:30:26 -0400

    envia e-mail pelo amazon ses

---
+++ src/config/mail.ts
@@ -0,0 +1,19 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+  defaults: {
+    from: {
+      email: 'robsonex@gmail.com',
+      name: 'Robson Souza',
+    },
+  },
+} as IMailConfig;
